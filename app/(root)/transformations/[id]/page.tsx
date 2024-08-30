9462bfe303d47be3ccffc97750ba2ad07e7def66
georgefrg <georgeftw00@gmail.com>
2024-08-30 19:15:54 +0300

    added type

--- app/(root)/transformations/[id]/page.tsx
+++ app/(root)/transformations/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = ({ params }) => {
+const ImageDetails = async ({ params: { id } }: SearchParamProps) => {
   return <div>page</div>;
 };
 
-export default page;
+export default ImageDetails;
